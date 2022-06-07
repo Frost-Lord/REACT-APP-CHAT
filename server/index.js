852a33169688a75b2a2796b603e354b283f6f966
!Glitched! <51778028+DaKing01@users.noreply.github.com>
2022-06-07 17:10:41 +1000

    code

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
 const app = express();
+const socket = require("socket.io");
 require("dotenv").config();
 
 app.use(cors());
@@ -19,6 +22,30 @@ mongoose
     console.log(err.message);
   });
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
