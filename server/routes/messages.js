43484833d636cf809387eaf573ba9032b582b59f
!Glitched! <51778028+DaKing01@users.noreply.github.com>
2022-06-07 18:20:41 +1000

    wrg

--- server/routes/messages.js
+++
@@ -1,7 +0,0 @@
-const { addMessage, getMessages } = require("../controllers/messageController");
-const router = require("express").Router();
-
-router.post("/addmsg/", addMessage);
-router.post("/getmsg/", getMessages);
-
-module.exports = router;
